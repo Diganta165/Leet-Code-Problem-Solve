396e2257c62d5ec00257755824c60b8fcfe85fdf
Diganta165 <mdsrdiganta@gmail.com>
2022-07-27 21:54:41 +0600

    35. Search Insert Position complete

--- 35. Search Insert Position/search_insert_position.js
+++ 35. Search Insert Position/search_insert_position.js
@@ -19,6 +19,7 @@ var searchInsert = function(nums, target) {
             return i--;
         };
     }
+    if(nums.indexOf(target)=== -1) return nums.length;
 };
 
-console.log(searchInsert([1,3,5,6], 7))
+console.log(searchInsert([1,3,5,6], 5))
