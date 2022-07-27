248372374372f2ddb3db8d1d33e6565c57e68043
Diganta165 <mdsrdiganta@gmail.com>
2022-07-27 21:55:25 +0600

    comeplete

--- 35. Search Insert Position/search_insert_position.js
+++ 35. Search Insert Position/search_insert_position.js
@@ -1,21 +1,9 @@
 var searchInsert = function(nums, target) {
-    let k = 0;
-    // for(let i = 0; i < nums.length; i++){
-    //     if(nums[i] === target) {
-    //         k++;
-    //         return i;
-    //     };
-
-
-    // }
-    // if(k === 0)
-
     let value = nums.indexOf(target);
 
     if(value != -1) return value;
      for(let i = 0; i < nums.length; i++){
         if(nums[i] > target) {
-            // k++;
             return i--;
         };
     }
