fd53ae2cd6b8cfbfd2419d93543fa27fdc443b7f
Diganta165 <mdsrdiganta@gmail.com>
2022-07-31 21:31:19 +0600

    67. add_binary proper solution

--- 67. Add Binary/add_binary.js
+++ 67. Add Binary/add_binary.js
@@ -8,4 +8,24 @@ var addBinary = function(a, b) {
     return ((num1+num2).toString(2));
 };
 
-addBinary("1010", "1011")
+addBinary("1010", "1011");
+
+
+// ****************************
+var addBinary = function(a, b) {
+    let sum = "";
+    let carry = 0;
+    while (a || b || carry) {
+        let add = +a.slice(-1) + +b.slice(-1) + carry;
+        if (add > 1) {
+            sum = (add % 2) + sum;
+            carry = 1;
+        } else {
+            sum = add + sum;
+            carry = 0;
+        }
+        a = a.slice(0, -1);
+        b = b.slice(0, -1);
+    }
+    return sum;
+};
