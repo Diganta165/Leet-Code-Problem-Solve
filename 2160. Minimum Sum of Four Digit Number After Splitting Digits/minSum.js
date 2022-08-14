b1e492ee8b8b1fba27bc8d1a1cb74796c47acdc7
unknown <mdsrdiganta@gmail.com>
2022-08-14 16:19:36 +0600

    2160. Minimum Sum of Four Digit Number After Splitting Digits complete

--- 2160. Minimum Sum of Four Digit Number After Splitting Digits/minSum.js
+++ 2160. Minimum Sum of Four Digit Number After Splitting Digits/minSum.js
@@ -1,27 +1,30 @@
 var minimumSum = function(num) {
-    num = num.toString()
-    // console.log(num)
-    let splitArr = num.split("");
-    // console.log(splitArr)
-    let pairArr = [];
-    let testValue = null;
+    num = num.toString().split("").sort();
+    // return parseInt(num[0]+ num[2]) + parseInt(num[1]+num[3]);
+    console.log(parseInt(num[0]+ num[2]) + parseInt(num[1]+num[3]));
+
+    console.log(num)
+    // let splitArr = num.split("");
+    // // console.log(splitArr)
+    // let pairArr = [];
+    // let testValue = null;
     
-    for(let i = 0; i < splitArr.length; i++){
-        for(let j = i+1; j < splitArr.length; j++){
-            testValue = splitArr[i] + splitArr[j];
-            pairArr.push(testValue);
-        }
-    }
-    console.log(pairArr)
-    let smallestValue = parseInt(pairArr[0])+ parseInt(pairArr[1]);
-    for(let i = 0; i < pairArr.length; i++){
-        for(let j = 1; j < pairArr.length; j++){
-            if(smallestValue > (parseInt(pairArr[i]) + parseInt(pairArr[j]))){
-                smallestValue = parseInt(pairArr[i]) + parseInt(pairArr[j]);
-            }
-        }
-    }
-    console.log(smallestValue)
+    // for(let i = 0; i < splitArr.length; i++){
+    //     for(let j = i+1; j < splitArr.length; j++){
+    //         testValue = splitArr[i] + splitArr[j];
+    //         pairArr.push(testValue);
+    //     }
+    // }
+    // console.log(pairArr)
+    // let smallestValue = parseInt(pairArr[0])+ parseInt(pairArr[1]);
+    // for(let i = 0; i < pairArr.length; i++){
+    //     for(let j = 1; j < pairArr.length; j++){
+    //         if(smallestValue > (parseInt(pairArr[i]) + parseInt(pairArr[j]))){
+    //             smallestValue = parseInt(pairArr[i]) + parseInt(pairArr[j]);
+    //         }
+    //     }
+    // }
+    // console.log(smallestValue)
 
 
         
