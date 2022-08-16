8d725a211258542dfa7f2c3c6ea02fda943b54c2
unknown <mdsrdiganta@gmail.com>
2022-08-16 11:49:48 +0600

    1512. Number of Good Pairs complete

--- 1512. Number of Good Pairs/numbreOfGoodPairs.js
+++ 1512. Number of Good Pairs/numbreOfGoodPairs.js
@@ -1,14 +1,16 @@
 var numIdenticalPairs = function(nums) {
     let goodPair = 0;
 
-    for(let i = 0; i < nums.length; i++){
-        for(let j = 1; j < nums.length-1; j++){
-            if(nums[i] === nums[j] && nums[i]< nums[j] ) goodPair++;
+    for(let i = 0; i <= nums.length-1; i++){
+        for(let j = 1; j <= nums.length-1; j++){
+            if(nums[i] == nums[j] && i < j ) goodPair++;
+            // if(nums[i] == nums[j]){goodPair++;}
         }
     }
 
     console.log(goodPair)
-    console.log(d3.pairs([nums]))
+    // console.log(d3.pairs([nums]))
 };
 
-numIdenticalPairs([1,2.3,1,1,3])
+numIdenticalPairs([1,2,3,1,1,3])
+numIdenticalPairs([1,1,1,1])
