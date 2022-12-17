e0f3cadc81912905df7408a7bb8d273e049a3ae3
Diganta165 <mdsrdiganta@gmail.com>
2022-12-17 02:25:45 -0800

    88. merge sorted array complete

--- 88. Merge Sorted Array/mergeSortedArray.js
+++ 88. Merge Sorted Array/mergeSortedArray.js
@@ -1,36 +1,22 @@
 var merge = function(nums1, m, nums2, n) {
-    nums1.length = m;
-    nums2.length = n;
     if(nums2?.length === 0) return;
+    let first = m-1;
+    let second = n-1;
+    let i = m + n -1;
 
-    // nums1?.forEach((elem1, index1) => {
-    //     nums2?.forEach((elem2, index2) =>{
-    //         // (elem2 > elem1)?  elem1.splice(index1, 0, elem2) : elem1.splice(index1-1, elem2);
-    //         if(elem2 > elem1){
-    //             nums1.splice(index1, 0, elem2);
-    //         }else{
-    //             nums1.splice(index1-1, elem2);
-    //         } 
-    //     });
-    // });
+    while(second >= 0){
+        let fVal = nums1[first];
+        let sVal = nums2[second];
 
-    for(let i = 0; i < m+n; i++){
-        for(let j = 0; j<n; j++ ){
-            // if(nums1[i] > nums2[j]){
-            //     nums1.splice(i, 0, nums2[j]);
-            // }else{
-            //     nums1.push(nums2[j])
-            // }
-            console.log("nums1[i]", nums1[i])
-            console.log("nums2[j]", nums2[j])
-            if(nums1[i] >nums2[j]){
-                nums1.splice(i, 0 , nums2[j]);
-                console.log("nums11", nums1)
-            }
+        if(fVal > sVal){
+            nums1[i] = fVal;
+            i--;
+            first --;
+        }else{
+            nums1[i] = sVal;
+            i--;
+            second--;
         }
     }
 
-    console.log(nums1)
 };
-
-merge([2,3, 23], 3, [1, 5], 2);
